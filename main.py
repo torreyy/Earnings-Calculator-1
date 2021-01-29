9a90bd64d1f91e46b1c8449ee395ff218f92a1c4
torreyy <aladyalways@outlook.com>
2021-01-29 03:01:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Your Earnings Calculator")
+
+hourly = float(input("How much do you make per hour? "))
+
+daily = float(input("How many hours do you work per day? "))
+
+daily_earnings = hourly * daily
+
+print("This is your daily earnings:", str(daily_earnings))
+
+weekly_earnings = daily_earnings * 5
+
+print("This is your weekly earnings: ", + daily_earnings * 5)
+
+monthly_earnings = daily_earnings *5 /12
+
+print("This is your monthly earnings: ", + weekly_earnings * 52 /12 )
+yearly_earnings = monthly_earnings * 12
+
+print("This is your yearly earnings: ", + weekly_earnings * 52)
+
+print("Congratulations!")
+
